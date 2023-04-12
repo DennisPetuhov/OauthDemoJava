0dc8f3593ab4355fe866118ca62cd1f86056e203
DennisPetuhov <kotlinkod@gmail.com>
2023-04-12 11:49:27 +0300

    Initial commit

---
+++ src/main/java/com/example/oauthdemojava/OauthDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthdemojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthDemoJavaApplication.class, args);
+    }
+
+}
